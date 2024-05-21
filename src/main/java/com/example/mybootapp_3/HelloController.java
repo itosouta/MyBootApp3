d35fb5dbe36bde7cf76c26cd3a9b47716802e2c1
itosouta <te210832@gmail.com>
2024-05-21 11:25:38 +0900

    課題3

---
+++ src/main/java/com/example/mybootapp_3/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.mybootapp_3;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+* Hello world!
+*/
+@RestController
+public class HelloController
+{
+    @RequestMapping("/")
+    public String index() {
+    return "Hello, Spring Boot!";
+    }
+    @RequestMapping("/te/taro")
+    public String miyagi() {
+    return "Taro desu!";
+    }
+}
